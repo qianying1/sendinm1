522cd6fe778c1df50dc0267b2ca2822a7310792e
qianhaibin <1101627399@qq.com>
2018-04-11 17:22:28 +0800

    second commit

---
+++ src/main/java/com/gzsendi/qhb/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.gzsendi.qhb.mapper;
+
+import com.gzsendi.qhb.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
