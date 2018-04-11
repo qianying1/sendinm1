cb34ef381416d59243a2d693e92c4a8434191424
qianhaibin <1101627399@qq.com>
2018-04-11 16:37:17 +0800

    second commit

---
+++ src/test/java/com/gzsendi/qhb/GzsendiNmApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gzsendi.qhb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GzsendiNmApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
